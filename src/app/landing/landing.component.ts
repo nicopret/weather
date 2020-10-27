b662ef35f32ce1a8d0325867fa5a07903be329e5
Nico Pretorius <niconchat@yahoo.com>
2020-10-27 14:59:35 +0000

    Updates to get ios to work (#5)
    Co-authored-by: Nico Pretorius <nico.pretorius@creditkarma.com>

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { isAndroid } from '@nativescript/core/platform';
 
 import { Page } from '@nativescript/core/ui/page';
 
-import * as geolocation from 'nativescript-geolocation';
+import * as androidLocation from 'nativescript-geolocation';
+import * as iosLocation from '@nativescript/geolocation';
 import { Accuracy } from '@nativescript/core/ui/enums';
 
 import { ApiService } from '../api/api.service';
@@ -28,12 +30,12 @@ export class LandingComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.enableLocation();
-        this.getLocation();
+        isAndroid ? this.enableAndroidLocation() : this.enableIosLocation();
+        isAndroid ? this.getAndroidLocation() : this.getIosLocation();
     }
 
-    getLocation = () => {
-        geolocation.getCurrentLocation({
+    getAndroidLocation = () => {
+        androidLocation.getCurrentLocation({
             desiredAccuracy: Accuracy.high,
             maximumAge: 5000,
             timeout: 10000
@@ -48,10 +50,40 @@ export class LandingComponent implements OnInit {
         })
     }
 
-    enableLocation = () => {
-        geolocation.isEnabled().then((isEnabled) => {
+    getIosLocation = () => {
+        iosLocation.getCurrentLocation({
+            desiredAccuracy: Accuracy.high,
+            maximumAge: 5000,
+            timeout: 10000
+        }).then(loc => {
+            if (loc) {
+                const { latitude, longitude } = loc;
+                this.apiService.current(latitude, longitude);
+                this.apiService.onecall(latitude, longitude);
+            }
+        }, (e) => {
+            console.log(`Error: ${e.message || e}`);
+        })
+    }
+
+    enableAndroidLocation = () => {
+        androidLocation.isEnabled().then((isEnabled) => {
             if (!isEnabled) {
-                geolocation.enableLocationRequest(true, true).then(() => {
+                androidLocation.enableLocationRequest(true, true).then(() => {
+                    console.log('User enabled location service');
+                }, (e) => {
+                    console.log(`Error: ${e.message || e}`);
+                }).catch(ex => {
+                    console.log('Unable to enable location', ex);
+                });
+            }
+        });
+    }
+
+    enableIosLocation = () => {
+        iosLocation.isEnabled().then((isEnabled) => {
+            if (!isEnabled) {
+                iosLocation.enableLocationRequest(true, true).then(() => {
                     console.log('User enabled location service');
                 }, (e) => {
                     console.log(`Error: ${e.message || e}`);
